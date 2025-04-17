531f06eb116e1856e05a1458103b9ba3989d6ca5
binny <binny3213@gmail.com>
2025-04-17 16:50:39 +0300

    Switch to environment-based DB config

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,11 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCEHMY_DATABASE_URL = 'postgresql://postgres:Bzs37#Bzs@localhost/TodoApplicationDatabase'
 
-engine = create_engine(SQLALCEHMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 Base = declarative_base()
